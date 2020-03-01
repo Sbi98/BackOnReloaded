08ac83e23e1e4b6a9b10eb353a0487641c6032e9
Giancarlo Sorrentino <giancarlosorrentino99@gmail.com>
2020-03-01 22:06:10 +0100

    Commentata una cosa che non faceva fare il login

--- BackOn/SceneDelegate.swift
+++ BackOn/SceneDelegate.swift
@@ -46,7 +46,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView.onTapGesture{window.endEditing(true)})
+            window.rootViewController = UIHostingController(rootView: contentView
+ //               .onTapGesture{window.endEditing(true)}
+            )
             self.window = window
             window.makeKeyAndVisible()
         }
