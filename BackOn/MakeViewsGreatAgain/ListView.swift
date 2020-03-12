793a0a6d6c19ac174b1ac6c70f1c52b2505ea760
Emanuele Triuzzi <emanueletriuzzi@gmail.com>
2020-03-12 12:44:31 +0100

    Finale listview

--- BackOn/MakeViewsGreatAgain/ListView.swift
+++ BackOn/MakeViewsGreatAgain/ListView.swift
@@ -19,42 +19,52 @@ struct ListView: View {
     var body: some View {
         ScrollView(Axis.Set.vertical, showsIndicators: true){
             VStack (alignment: .leading){
+                if(mode == RequiredBy.DiscoverTab){
+                    Text("Around you")
+                        .fontWeight(.bold)
+                        .font(.title)
+                        .padding(.leading, 15)
+                }
                 
-                if(mode == RequiredBy.DiscoverTab || mode == RequiredBy.TaskTab) {
-                    
-                    if(mode == RequiredBy.DiscoverTab){
-                        Text("Around you")
-                            .fontWeight(.bold)
-                            .font(.title)
-                            .padding(.leading, 15)
-                    }
-                    
-                    ForEach(mode == RequiredBy.DiscoverTab ? shared.discoverablesArray() : shared.tasksArray(), id: \.ID) { current in
-                        Button(action: {
-                            self.discoverTabController.selectedTask = current
-                            self.discoverTabController.showModal = true
-                        }) {
-                            VStack(alignment: .leading){
-                                HStack{
+                ForEach(mode == RequiredBy.DiscoverTab ? shared.discoverablesArray() : mode == RequiredBy.RequestDetailedModal ? shared.requestsArray() : shared.tasksArray(), id: \.ID) { current in
+                    Button(action: {
+                        self.discoverTabController.selectedTask = current
+                        self.discoverTabController.showModal = true
+                    }) {
+                        VStack(alignment: .leading){
+                            HStack{
+                                if(self.mode == RequiredBy.RequestDetailedModal){
+                                    Avatar(image: current.helperUser != nil ? current.helperUser!.profilePic : nil)
+                                }
+                                else {
                                     Avatar(image: current.neederUser.profilePic)
-                                    VStack (alignment: .leading){
+                                }
+                                VStack (alignment: .leading){
+                                    if(self.mode == RequiredBy.RequestDetailedModal){
+                                        Text(current.helperUser != nil ? current.helperUser!.identity : "Still nobody")
+                                            .font(Font.custom("SF Pro Text", size: 26))
+                                            .fontWeight(.regular)
+                                            .offset(x: 0, y: -3)
+                                            .lineLimit(1)
+                                    }
+                                    else {
                                         Text(current.neederUser.identity)
                                             .font(Font.custom("SF Pro Text", size: 26))
                                             .fontWeight(.regular)
                                             .offset(x: 0, y: -3)
                                             .lineLimit(1)
-                                        
-                                        Text(current.title)
-                                            .font(.subheadline)
-                                            .fontWeight(.light)
-                                            .offset(x: 0, y: 1)
-                                            .lineLimit(2)
-                                        
-                                    }.padding(.leading, 5)
-                                    Spacer()
-                                }
-                                HStack {
-                                Text("\(current.city)")
+                                    }
+                                    Text(current.title)
+                                        .font(.subheadline)
+                                        .fontWeight(.light)
+                                        .offset(x: 0, y: 1)
+                                        .lineLimit(2)
+                                    
+                                }.padding(.leading, 5)
+                                Spacer()
+                            }
+                            HStack {
+                                Text("\(self.mode == RequiredBy.RequestDetailedModal ? current.address : current.city)")
                                     .foregroundColor(.secondary)
                                     .fontWeight(.regular)
                                     .font(Font.custom("SF Pro Text", size: 17))
@@ -63,63 +73,13 @@ struct ListView: View {
                                     .foregroundColor(.secondary)
                                     .fontWeight(.regular)
                                     .font(Font.custom("SF Pro Text", size: 17))
-                                }
-                            }.padding(15)
-                        }.buttonStyle(PlainButtonStyle())
+                            }
+                        }.padding(15)
+                    }.buttonStyle(PlainButtonStyle())
                         .background(
                             RoundedRectangle(cornerRadius: 15, style: .continuous)
-                            .fill(
-                                LinearGradient(gradient: Gradient(colors: [Color(UIColor(#colorLiteral(red: 0.9450980392, green: 0.8392156863, blue: 0.6705882353, alpha: 1))), Color(UIColor(#colorLiteral(red: 0.9450980392, green: 0.8392156863, blue: 0.6705882353, alpha: 1)))]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                            )
-                        )
-                    }
-                }//FINE IF DISCOVER o TASK
-                
-                if(mode == RequiredBy.RequestDetailedModal) {
-                    ForEach(shared.requestsArray(), id: \.ID) { current in
-                        Button(action: {
-                            self.discoverTabController.selectedTask = current
-                            self.discoverTabController.showModal = true
-                        }) {
-                            VStack(alignment: .leading){
-                                HStack{
-                                    Avatar(image: current.helperUser != nil ? current.helperUser!.profilePic : nil)
-                                    VStack (alignment: .leading){
-                                        Text(current.helperUser != nil ? current.helperUser!.identity : "Still nobody")
-                                            .font(Font.custom("SF Pro Text", size: 26))
-                                            .fontWeight(.regular)
-                                            .offset(x: 0, y: -3)
-                                            .lineLimit(1)
-                                        
-                                        Text(current.title)
-                                            .font(.subheadline)
-                                            .fontWeight(.light)
-                                            .offset(x: 0, y: 1)
-                                            .lineLimit(2)
-                                        
-                                    }.padding(.leading, 5)
-                                    Spacer()
-                                }
-                                HStack{
-                                    Text(current.address)
-                                        .foregroundColor(.secondary)
-                                        .fontWeight(.regular)
-                                        .font(Font.custom("SF Pro Text", size: 17))
-                                    Spacer()
-                                    Text("\(current.date, formatter: customDateFormat)")
-                                        .foregroundColor(.secondary)
-                                        .fontWeight(.regular)
-                                        .font(Font.custom("SF Pro Text", size: 17))
-                                }
-                            }.padding(15)
-                        }.buttonStyle(PlainButtonStyle())
-                        .background(
-                            RoundedRectangle(cornerRadius: 15, style: .continuous)
-                            .fill(
-                                LinearGradient(gradient: Gradient(colors: [Color(UIColor(#colorLiteral(red: 0.9450980392, green: 0.8392156863, blue: 0.6705882353, alpha: 1))), Color(UIColor(#colorLiteral(red: 0.9450980392, green: 0.8392156863, blue: 0.6705882353, alpha: 1)))]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                            )
-                        )
-                    }
+                                .fill(LinearGradient(gradient: Gradient(colors: [Color(UIColor(#colorLiteral(red: 0.9450980392, green: 0.8392156863, blue: 0.6705882353, alpha: 1))), Color(UIColor(#colorLiteral(red: 0.9450980392, green: 0.8392156863, blue: 0.6705882353, alpha: 1)))]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                    )
                 }
                 Spacer()
             }
