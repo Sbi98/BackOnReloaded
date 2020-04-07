1da37c55e35776ccf06c727f52ede9aa97aee40f
Sbi98 <ricciovi@hotmail.it>
2020-04-07 18:53:25 +0200

    piccolo fix

--- BackOn/MakeViewsGreatAgain/ListView.swift
+++ BackOn/MakeViewsGreatAgain/ListView.swift
@@ -28,7 +28,7 @@ struct ListView: View {
                     .buttonStyle(PlainButtonStyle())
                     .background(RoundedRectangle(cornerRadius: 15, style: .continuous).fill(Color(UIColor(#colorLiteral(red: 0.9450980392, green: 0.8392156863, blue: 0.6705882353, alpha: 1)))))   
                 }
-                if (mode == .RequestViews && !shared.myExpiredRequests.isEmpty) || (mode == .TaskViews && !shared.myExpiredRequests.isEmpty){
+                if (mode == .RequestViews && !shared.myExpiredRequests.isEmpty) || (mode == .TaskViews && !shared.myExpiredTasks.isEmpty){
                     Divider()
                     ForEach(shared.arrayFromSet(mode: mode, expiredSet: true), id: \._id) { current in
                         Button(action: {
