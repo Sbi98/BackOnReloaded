d1a9d1759cd269d4f725d6fb191e0626d2630f2b
Giancarlo Sorrentino <giancarlosorrentino99@gmail.com>
2020-04-09 21:48:27 +0200

    Aggiunta del parametro deviceID in database controller

--- BackOn/Backend/DatabaseController.swift
+++ BackOn/Backend/DatabaseController.swift
@@ -126,7 +126,7 @@ class DatabaseController {
     static func signUp(name: String, surname: String?, email: String, photoURL: URL, completion: @escaping (User?, ErrorString?)-> Void) {
         do {
             print("*** DB - \(#function) ***")
-            let parameters: [String: String] = ["name": name, "surname": surname ?? "", "email" : email, "photo": "\(photoURL)"]
+            let parameters: [String: String] = ["name": name, "surname": surname ?? "", "email" : email, "photo": "\(photoURL)", "deviceToken": deviceToken]
             let request = initJSONRequest(urlString: ServerRoutes.signUp, body: try JSONSerialization.data(withJSONObject: parameters))
             URLSession.shared.dataTask(with: request) { data, response, error in
                 guard error == nil else {return completion(nil, "Error in " + #function + ". The error is:\n\(error!.localizedDescription)")}
